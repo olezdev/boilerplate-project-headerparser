816fbb57b2e06f0c5c5181a474fed53623f11f84
Leonardo Olivera <olezdev@gmail.com>
2023-12-13 13:55:59 -0300

    add endpoint whoami

--- index.js
+++ index.js
@@ -3,11 +3,27 @@
 
 // init project
 require('dotenv').config();
-var express = require('express');
-var app = express();
+const express = require('express');
+const app = express();
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC
+const enableCORS = (req, res, next) => {
+  if (!process.env.DISABLE_XORIGIN) {
+    const allowedOrigins = ["https://www.freecodecamp.org"];
+    const origin = req.headers.origin;
+    if (!process.env.XORIGIN_RESTRICT || allowedOrigins.indexOf(origin) > -1) {
+      console.log(req.method);
+      res.set({
+        "Access-Control-Allow-Origin": origin,
+        "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
+        "Access-Control-Allow-Headers": "Origin, X-Requested-With, Content-Type, Accept"
+      });
+    }
+  }
+  next();
+}
+
 var cors = require('cors');
 app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
 
@@ -15,16 +31,35 @@ app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 2
 app.use(express.static('public'));
 
 // http://expressjs.com/en/starter/basic-routing.html
-app.get('/', function (req, res) {
+app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html');
 });
 
 // your first API endpoint...
-app.get('/api/hello', function (req, res) {
+app.get('/api/hello', (req, res) => {
   res.json({ greeting: 'hello API' });
 });
 
+// ipaddress":"::ffff:159.20.14.100","language":"en-US,en;q=0.5",<br>"software":"Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:50.0) Gecko/20100101 Firefox/50.0
+// app.use((req, res, next) => {
+//   req.language = req.headers['accept-language'];
+//   req.software = req.headers['user-agent'];
+//   next();
+// })
+
+app.get('/api/whoami', (req, res) => {
+  const ipaddress = req.ip;
+  const language = req.headers['accept-language'];
+  const software = req.headers['user-agent'];
+
+  const clientInfo = {
+    ipaddress, language, software
+  }
+  res.json(clientInfo);
+});
+
 // listen for requests :)
-var listener = app.listen(process.env.PORT || 3000, function () {
+const port = process.env.PORT || 3000
+const listener = app.listen(port, () => {
   console.log('Your app is listening on port ' + listener.address().port);
 });
