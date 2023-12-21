b0db469fd3f53182658ed6a8e9efb05826923ca0
Leonardo Olivera <olezdev@gmail.com>
2023-12-21 10:55:16 -0300

    edit public express.static

--- index.js
+++ index.js
@@ -1,34 +1,13 @@
-// index.js
-// where your node app starts
-
-// init project
-require('dotenv').config();
 const express = require('express');
 const app = express();
+require('dotenv').config();
 
-// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC
-const enableCORS = (req, res, next) => {
-  if (!process.env.DISABLE_XORIGIN) {
-    const allowedOrigins = ["https://www.freecodecamp.org"];
-    const origin = req.headers.origin;
-    if (!process.env.XORIGIN_RESTRICT || allowedOrigins.indexOf(origin) > -1) {
-      console.log(req.method);
-      res.set({
-        "Access-Control-Allow-Origin": origin,
-        "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
-        "Access-Control-Allow-Headers": "Origin, X-Requested-With, Content-Type, Accept"
-      });
-    }
-  }
-  next();
-}
-
-var cors = require('cors');
+const cors = require('cors');
 app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
-app.use(express.static('public'));
+const path = require('path');
+app.use(express.static(path.join(__dirname, 'public')))
 
 // http://expressjs.com/en/starter/basic-routing.html
 app.get('/', (req, res) => {
@@ -52,7 +31,7 @@ app.get('/api/whoami', (req, res) => {
 });
 
 // listen for requests :)
-const port = process.env.PORT || 3000
-const listener = app.listen(port, () => {
-  console.log('Your app is listening on port ' + listener.address().port);
+const PORT = process.env.PORT || 3000
+app.listen(PORT, () => {
+  console.log(`Your app is listening on port ${PORT}`);
 });
